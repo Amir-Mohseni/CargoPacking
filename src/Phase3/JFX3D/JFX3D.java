e5b5566359d71b79b639053a793c06629795bc61
KoungMyatHein <k.koungmyathein@student.maastrichtuniversity.nl>
2024-01-12 16:02:48 +0100

    Refactor JFX3D class to add new features
    This commit restructures JFX3D class by adding mouse control and 3D rotation capabilities. The application scene has been enhanced to include perspective camera, subscene and mouse event bindings. A RotatorGroup with X and Y axis rotation methods has also been implemented for advanced 3D interactions.

--- src/Phase3/JFX3D/JFX3D.java
+++ src/Phase3/JFX3D/JFX3D.java
@@ -1,51 +1,122 @@
 package Phase3.JFX3D;
 
 import javafx.application.Application;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.event.EventHandler;
 import javafx.scene.*;
+import javafx.scene.input.ScrollEvent;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.*;
 import javafx.scene.shape.Box;
+import javafx.scene.transform.Rotate;
+import javafx.scene.transform.Transform;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class JFX3D extends Application {
+    public static final int WIDTH = 800, HEIGHT = 600;
+
+    private final DoubleProperty angleX = new SimpleDoubleProperty(0);
+    private final DoubleProperty angleY = new SimpleDoubleProperty(0);
+    private double anchorX, anchorY;
+    private double anchorAngleX = 0, anchorAngleY = 0;
 
     @Override
     public void start(Stage primaryStage) {
-        // Creating a Box
-        Box box = new Box();
+        BorderPane bp = new BorderPane();
+        RotatorGroup rg = new RotatorGroup();
 
-        // Setting the properties of the Box
-        box.setWidth(150.0);
-        box.setHeight(150.0);
-        box.setDepth(150.0);
+        Scene mainScene = new Scene(bp, WIDTH, HEIGHT);
 
-        box.setTranslateX(100);
-        box.setTranslateY(100);
-        box.setTranslateZ(10);
+        PerspectiveCamera perspectiveCamera = new PerspectiveCamera(true);
 
-        // Creating a Group
-        Group root = new Group(box);
+        SubScene scene = new SubScene(rg, WIDTH, HEIGHT, true, null);
 
-        // Creating a scene with specified width, height and fill color
-        Scene scene = new Scene(root, 600, 300, Color.LIGHTBLUE);
+        bp.setCenter(scene);
 
-        // Enable 3D rotation for the cube
-        PerspectiveCamera camera = new PerspectiveCamera(false);
-        camera.setTranslateX(0);
-        camera.setTranslateY(0);
-        camera.setTranslateZ(-300);
+        scene.setFill(Color.LIGHTGRAY);
+        scene.setCamera(perspectiveCamera);
 
-        // Add the camera to the scene
-        scene.setCamera(camera);
+        perspectiveCamera.translateXProperty().set(0);
+        perspectiveCamera.translateYProperty().set(0);
+        perspectiveCamera.translateZProperty().set(-3000);
+        perspectiveCamera.setNearClip(1);
+        perspectiveCamera.setFarClip(100000);
 
-        // Setting the title to Stage.
-        primaryStage.setTitle("Sample Application");
+        initMouseControl(rg, scene, primaryStage);
 
-        // Adding the scene to Stage
-        primaryStage.setScene(scene);
+        Box newBox = new Box();
+        newBox.setWidth(150);
+        newBox.setHeight(150);
+        newBox.setDepth(150);
 
-        // Displaying the contents of the stage
+        rg.getChildren().add(newBox);
+
+        primaryStage.setTitle("Project 1 phase 3");
+        primaryStage.setScene(mainScene);
         primaryStage.show();
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override public void handle(WindowEvent t) {
+                System.exit(0);
+            }
+        });
+
     }
+
+    class RotatorGroup extends Group {
+        Rotate r;
+        Transform t = new Rotate();
+        /**
+         * Rotate around the x-axis
+         * @param ang
+         */
+        void rotateX(int ang) {
+            r = new Rotate(ang, Rotate.X_AXIS);
+            t = t.createConcatenation(r);
+            this.getTransforms().clear();
+            this.getTransforms().add(t);
+        }
+        /**
+         * Rotates around the Y-axis
+         * @param ang
+         */
+        void rotateY(int ang) {
+            r = new Rotate(ang, Rotate.Y_AXIS);
+            t = t.createConcatenation(r);
+            this.getTransforms().clear();
+            this.getTransforms().add(t);
+        }
+    }
+
+    private void initMouseControl(RotatorGroup group, SubScene scene, Stage stage) {
+        Rotate xRotate;
+        Rotate yRotate;
+        group.getTransforms().addAll(
+                xRotate = new Rotate(0, Rotate.X_AXIS),
+                yRotate = new Rotate(0, Rotate.Y_AXIS)
+        );
+        xRotate.angleProperty().bind(angleX);
+        yRotate.angleProperty().bind(angleY);
+
+        scene.setOnMousePressed(event -> {
+            anchorX = event.getSceneX();
+            anchorY = event.getSceneY();
+            anchorAngleX = angleX.get();
+            anchorAngleY = angleY.get();
+        });
+
+        scene.setOnMouseDragged(event -> {
+            angleX.set(anchorAngleX - (anchorY - event.getSceneY()));
+            angleY.set(anchorAngleY + (anchorX - event.getSceneX()));
+        });
+
+        stage.addEventHandler(ScrollEvent.SCROLL, event -> {
+            double movement = -event.getDeltaY();
+            group.translateZProperty().set(group.getTranslateZ() + movement);
+        });
+    }
+
 
     public static void main(String args[]) {
         launch(args);
